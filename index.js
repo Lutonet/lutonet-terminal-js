c90f7d75f22bdb77fec5a0e71f4e40141705b048
afrowaveltd <65894408+afrowaveltd@users.noreply.github.com>
2022-03-14 00:05:14 +0000

    cleanup

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import Terminal from "./Terminal.js";
-import convert from "./functions/jsonToHtml.js";
-const terminal = new Terminal({ terminalType: "as-400" });
+const terminal = new Terminal();
 terminal.theme = "dark";
 terminal.start();
+
 terminal.displayTitle = true;
